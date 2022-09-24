bd3402bed1f4b84429fdcec16755b6bf43456c62
HaraT0323 <alra9si916323@gmail.com>
2022-09-24 23:01:13 +0900

    任意ポートで起動できるように対応

--- main.go
+++ main.go
@@ -4,20 +4,32 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen poet %s: %v", p, err)
+	}
+	if err := run(context.Background(), l); err != nil {
 		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
@@ -27,7 +39,7 @@ func run(ctx context.Context) error {
 	eg.Go(func() error {
 		// http.ErrServerClosed は
 		// http.Server.Shutdown() が正常に終了したことを示すので異常ではない。
-		if err := s.ListenAndServe(); err != nil &&
+		if err := s.Serve(l); err != nil &&
 			err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
