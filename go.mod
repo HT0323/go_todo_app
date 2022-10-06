84ca35bc5fdfa09eb14ef9a80d34675e1b29e182
HaraT0323 <alra9si916323@gmail.com>
2022-10-06 22:54:17 +0900

    ルーテイング定義を追加

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.18
 
 require golang.org/x/sync v0.0.0-20220907140024-f12130a52804
 
-require github.com/caarlos0/env/v6 v6.10.1 // indirect
+require github.com/caarlos0/env/v6 v6.10.1
--- main.go
+++ main.go
@@ -5,13 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/HT0323/go_todo_app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -22,8 +18,6 @@ func main() {
 }
 
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -34,28 +28,7 @@ func run(ctx context.Context) error {
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello!!!, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-	// 別ゴルーチンでHTTPサーバーを起動する
-	eg.Go(func() error {
-		// http.ErrServerClosed は
-		// http.Server.Shutdown() が正常に終了したことを示すので異常ではない。
-		if err := s.Serve(l); err != nil &&
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-	// チャネルからの通知（終了通知）を待機する
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-	// Goメソッドで起動した別ゴルーチンの終了を待つ。
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
