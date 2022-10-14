742305e95d278a48e5ea13a2ac55c8f9f725b1d3
HaraT0323 <alra9si916323@gmail.com>
2022-10-14 21:03:51 +0900

    configのデフォルト値を使ったテストを追加

--- config/config_test.go
+++ config/config_test.go
@@ -47,3 +47,39 @@ func TestSetEnvNew(t *testing.T) {
 		t.Errorf("want %s, but %s", wantDbName, got.DBName)
 	}
 }
+
+func TestDefaultEnvNew(t *testing.T) {
+	wantPort := 8080
+	wantEnv := "dev"
+	wantDbHost := "127.0.0.1"
+	wantDbPort := 33306
+	wantDbUser := "todo"
+	wantDbPassword := "todo"
+	wantDbName := "todo"
+
+	got, err := New()
+	if err != nil {
+		t.Fatalf("cannot create config: %v", err)
+	}
+	if got.Port != wantPort {
+		t.Errorf("want %d, but %d", wantPort, got.Port)
+	}
+	if got.Env != wantEnv {
+		t.Errorf("want %s, but %s", wantEnv, got.Env)
+	}
+	if got.DBHost != wantDbHost {
+		t.Errorf("want %s, but %s", wantDbHost, got.DBHost)
+	}
+	if got.DBPort != wantDbPort {
+		t.Errorf("want %d, but %d", wantDbPort, got.DBPort)
+	}
+	if got.DBUser != wantDbUser {
+		t.Errorf("want %s, but %s", wantDbUser, got.DBUser)
+	}
+	if got.DBPassword != wantDbPassword {
+		t.Errorf("want %s, but %s", wantDbPassword, got.DBPassword)
+	}
+	if got.DBName != wantDbName {
+		t.Errorf("want %s, but %s", wantDbName, got.DBName)
+	}
+}
