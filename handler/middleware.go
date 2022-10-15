bad6248f8fbd294c6ca5b1c2e97bfc61d81cbd33
HaraT0323 <alra9si916323@gmail.com>
2022-10-15 23:45:11 +0900

    認証middlewareの追加

---
+++ handler/middleware.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/HT0323/go_todo_app/auth"
+)
+
+func AuthMiddleware(j *auth.JWTer) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			req, err := j.FillContext(r)
+			if err != nil {
+				RespondJSON(r.Context(), w, ErrResponse{
+					Message: "not find auth info",
+					Details: []string{err.Error()},
+				}, http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(w, req)
+		})
+	}
+}
+
+func AdminMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !auth.IsAdmin(r.Context()) {
+			RespondJSON(r.Context(), w, ErrResponse{
+				Message: "not admin",
+			}, http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+
+}
